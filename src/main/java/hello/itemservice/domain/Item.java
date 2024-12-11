b3ffe592d5e86c689fcb15060f026156009ce3aa
legoChoi <chlx159@gmail.com>
2024-12-11 11:51:11 +0900

    JPA 기본

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,22 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+//@Table 테이블명 같으면 생략가능
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 10) // 컬럼명 같으면 생략가능, 컬럼명은 언더스코어로 자동으로 변환
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    // JPA는 생성자가 필수
     public Item() {
     }
 
---
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -0,0 +1,83 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@Transactional // JPA의 모든 데이터 변경은 트랜잭션 안에서 이루어짐, 조회 X
+@AllArgsConstructor
+public class JpaItemRepository implements ItemRepository {
+
+    private final EntityManager em;
+
+    @Override
+    public Item save(Item item) {
+        em.persist(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = em.find(Item.class, itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+        // 트랜잭션 커밋되는 시점에 업데이트 쿼리 자동으로 날림
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Item item = em.find(Item.class, id);
+        return Optional.ofNullable(item);
+    }
+
+    // 동적 쿼리
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String jpql = "select i from Item i";
+        Integer maxPrice = cond.getMaxPrice();
+        String itemName = cond.getItemName();
+
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            jpql += " where";
+        }
+
+        boolean andFlag = false;
+
+        if (StringUtils.hasText(itemName)) {
+            jpql += " i.itemName like concat('%',:itemName,'%')";
+            andFlag = true;
+        }
+
+        if (maxPrice != null) {
+            if (andFlag) {
+                jpql += " and";
+            }
+            jpql += " i.price <= :maxPrice";
+        }
+
+        log.info("jpql={}", jpql);
+
+        TypedQuery<Item> query = em.createQuery(jpql, Item.class);
+        if (StringUtils.hasText(itemName)) {
+            query.setParameter("itemName", itemName);
+        }
+        if (maxPrice != null) {
+            query.setParameter("maxPrice", maxPrice);
+        }
+        return query.getResultList();
+    }
+}
